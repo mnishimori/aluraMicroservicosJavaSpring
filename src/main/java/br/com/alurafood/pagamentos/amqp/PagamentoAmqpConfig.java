d5df4023e0fabc3802df2cd2c4704e09e8075b8e
mnishimori <mnishimori@yahoo.com.br>
2022-11-28 18:27:36 -0300

    feature: configuração e publicação de mensagens para o RabbitMq

---
+++ src/main/java/br/com/alurafood/pagamentos/amqp/PagamentoAmqpConfig.java
@@ -0,0 +1,30 @@
+package br.com.alurafood.pagamentos.amqp;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PagamentoAmqpConfig {
+
+    @Bean
+    public Queue criarFila(){
+        return QueueBuilder.nonDurable("pagamento.concluido").build();
+    }
+
+    @Bean
+    public RabbitAdmin criarRabbitAdmin(ConnectionFactory conn){
+        return new RabbitAdmin(conn);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> inicializarAdmin(RabbitAdmin rabbitAdmin){
+        return event -> rabbitAdmin.initialize();
+    }
+
+}
